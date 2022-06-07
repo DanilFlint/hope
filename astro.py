e8fa846b3a4d593ba4594e61f7fa00c3aeab1f2f
DanilFlint <didenkod2010@gmail.com>
2022-06-07 12:22:07 +0300

    Добавлена ось Z

--- astro.py
+++ astro.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from polynomial_regression import regression
 import random
 
+
 def read(path):
     f = open(path)  # Заменить на нужное имя файла. Файл подаётся без шапки и конца.
     # Только со значениями. Пример в репозитории
@@ -24,78 +25,82 @@ def read(path):
     return array
 
 def function(X, B):
-    return B[0] + B[1]*X[:,1]+B[2]*X[:,2]+B[3]*X[:,3]+B[4]*X[:,4]
-def get_coord(X, B):
+    return B[0]+B[1]*X[:,1]+B[2]*X[:,2]+B[3]*X[:,3]+B[4]*X[:,4]
 
+def get_coord_el(X, B):
     #Для получения координат необходимо решить квадратное уравнение
     D = (B[3]*X[:,1] + B[2])**2 - 4*B[4]*(B[1]*X[:,1] + X[:,1]**2 + B[0])
     Y1 =  np.array([[i] for i in (-(B[3]*X[:,1] + B[2]) - D**(1/2))/(2*B[4])])
     Y2 =  np.array([[i] for i in (-(B[3]*X[:,1] + B[2]) + D**(1/2))/(2*B[4])])
 
-    bottom = np.array(np.concatenate((X[:,[1]], Y1), axis=1))
-    up = np.array(np.concatenate((X[:,[1]], Y2), axis=1))
-    ans = np.array(np.concatenate((bottom, up), axis=0))
+    print(np.array(np.concatenate((X[:,[1]],X[:, [2]], Y1,Y2), axis=1)))
+    ans = np.array(np.concatenate((X[:,[1]], Y1), axis=1))
+    ans = np.array(np.concatenate((ans, Y2), axis=1))
     return ans
+
+def get_coord_plate(X, A):
+    #Координаты Z
+
+    return -(A[0] + A[1] * X[:, 1] + A[2] * X[:, 2])
 
 
 if __name__ == '__main__':
-
+    #Начало региона данных
     coords = np.array(read('./Входные данные/kviila2.txt'))[:,[0,1]]/10000000 #Для меньшего размера графика делим.
+    coords_Z = np.array(read('./Входные данные/kviila2.txt'))[:,[2]]/10000000 #Для меньшего размера графика делим.
     mpl.rcParams['legend.fontsize'] = 10
-
     #X = np.array([coords[:,0], coords[:,1]])
-    X = np.concatenate((np.ones((coords.shape[0], 1)), coords), axis=1)  # Матрицана влияющих величин с единицами
+    ellipse_matrix = np.concatenate((np.ones((coords.shape[0], 1)), coords), axis=1)  # Матрицана влияющих величин с единицами
+    plane_matrix = np.concatenate((np.ones((coords.shape[0], 1)), coords), axis=1)
 
-    noise_X = np.array([random.uniform(-1, 1) for i in range(X.shape[0])])#Шум по X
-    noise_Y = np.array([random.uniform(-1, 1) for i in range(X.shape[0])])#Шум по Y
+    noise_X = np.array([random.uniform(-1, 1) for i in range(ellipse_matrix.shape[0])])#Шум по X
+    noise_Y = np.array([random.uniform(-1, 1) for i in range(ellipse_matrix.shape[0])])#Шум по Y
+    noise_Z = np.array([random.uniform(-0.2, 0.2) for i in range(ellipse_matrix.shape[0])])#Шум по Z
 
-    dis_X = np.amax(abs(noise_X))/abs(np.amax(abs(X[:, 1])))*100
-    dis_Y = np.amax(abs(noise_Y))/abs(np.amax(abs(X[:, 2])))*100
+    dis_X = np.amax(abs(noise_X)) / abs(np.amax(abs(ellipse_matrix[:, 1]))) * 100
+    dis_Y = np.amax(abs(noise_Y)) / abs(np.amax(abs(ellipse_matrix[:, 2]))) * 100
+    dis_Z = np.amax(abs(noise_Z)) / abs(np.amax(abs(coords_Z)))*100
 
-    X[:, 1] += noise_X
-    X[:, 2] += noise_Y
+    ellipse_matrix[:, 1] += noise_X
+    ellipse_matrix[:, 2] += noise_Y
+    coords_Z[:,0] += noise_Z
+    #Конец региона данных
 
-    print(dis_X)
-    print(dis_Y)
+    #Регион формирования матриц наблюдений
 
-    mul = np.array([[i] for i in X[:,1]*X[:,2]])# произведение координат x и y
-    X = np.concatenate((X, mul), axis=1)
+    #Матрица для параметров эллипса
+    mul = np.array([[i] for i in ellipse_matrix[:, 1] * ellipse_matrix[:, 2]])# произведение координат x и y
+    ellipse_matrix = np.concatenate((ellipse_matrix, mul), axis=1)
+    mul = np.array([[i] for i in ellipse_matrix[:, 2] * ellipse_matrix[:, 2]]) # квадрат координат y
+    ellipse_matrix = np.concatenate((ellipse_matrix, mul), axis=1)
 
-    mul = np.array([[i] for i in X[:,2]*X[:,2]]) # квадрат координат y
-    X = np.concatenate((X, mul), axis=1)
+    #Матрица для параметров плоскости составлена, добавлений не требуется
 
-    B = regression(X, (np.array([[i] for i in -X[:,1]**2])))
+    A = regression(plane_matrix, -coords_Z)
+    B = regression(ellipse_matrix, (np.array([[i] for i in -ellipse_matrix[:, 1] ** 2])))
 
-    R = function(X, B)
+    #R = function(ellipse_matrix, B)
 
-    ans = get_coord(X,B)
+    ans = get_coord_el(ellipse_matrix, B)
+
+    ans_plate = get_coord_plate(plane_matrix, A)
 
     sum_incline = 0
-    for i in (ans[732:,0] - X[:,1]):
+    for i in (ans[:,0] - ellipse_matrix[:, 1]):
         if not np.isnan(i):
             sum_incline+=i**2
     #print("Сумма квадратов разности : ", sum_incline)
 
-    print("Вычисленные параметры: ")
+    print("Вычисленные параметры A: ")
+    print(A)
+    print("Вычисленные параметры B: ")
     print(B)
-
-    #Вывод 2Д графика
-    fig = plt.figure('Орбита астероида 9162 Kwiila (1987 OA)')
-    plt.scatter(X[:,1], X[:,2], label='Орбита астероида 9162 Kwiila (1987 OA)', s=1)
-    plt.scatter(ans[:,0], ans[:,1], label='Вычисленная орбита астероида 9162 Kwiila (1987 OA)', s=1)
-    plt.legend()
-
-    solve_Y_lenear = function(X,B)
-
-    x = np.arange(1, X.shape[0] + 1, 1)
-
 
     #Вывод графика
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    theta = np.linspace(-50000 * np.pi, 50000 * np.pi, 10000000)
-    ax.scatter(X[:,1], X[:,2], -X[:,1]**2, label='Экспериментальные данные',s=0.7)
-    ax.scatter(X[:,1], X[:,2], solve_Y_lenear, label='Вычисленные данные',s=0.7)
+    ax.scatter(plane_matrix[:, 1], ellipse_matrix[:, 2], coords_Z, label='Экспериментальные данные', s=0.7)
+    ax.scatter(ans[:, 0], ellipse_matrix[:, 2], ans_plate, label='Вычисленные данные',s=0.7)
     ax.legend()
 
     plt.show()
